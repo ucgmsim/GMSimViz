13230a1e39de0438a9fcc1ca9760409e56a65254
Sung Eun Bae <sung.bae@canterbury.ac.nz>
2017-03-24 11:35:23 +1300

    upload and download instructions given

--- download.sh
+++ download.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-mydir=`dirname $0`
+mydir=`dirname $0
+if [[ $# -ne 3 ]]; then
+    echo "Usage: $0 local_dir fitzroy_dir fitzroy_username"
+    echo "   Eg. $0 /home/seb56 /a/b/x baes"
+    echo "       This program creates /home/seb/x and copies everything under x recursively"
+    exit 1
+fi
+
 mpirun -np 8 python2 $mydir/parallel_download.py $1 $2 $3
 
 
--- upload.sh
+++ upload.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 mydir=`dirname $0`
+if [[ $# -ne 3 ]]; then
+    echo "Usage: $0 local_dir fitzroy_dir fitzroy_username"
+    echo "   Eg. $0 ./x /home/baes baes"
+    echo "       where abspath of x is /a/b/x"
+    echo "       This program creates /home/baes/x and copies everything under x recursively"
+    exit 1
+fi
 mpirun -np 4 python2 $mydir/parallel_upload.py $1 $2 $3
 
 
