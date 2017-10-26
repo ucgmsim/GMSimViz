5f09a23091a37f8585c62a802cc39e81d3a4857e
jonney <shot082@gmail.com>
2017-10-26 15:41:47 +1300

    revert the change on gen_cords

--- gen_cords.py
+++ gen_cords.py
@@ -1,148 +1,91 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 import os
 from subprocess import check_call
 import sys
 import os.path
 
-sys.path.append(os.path.abspath(os.path.curdir))
+# should already be in PYTHONPATH (qcore library)
 from shared import *
 
-import load_config as ldcfg
-qcore_cfg=ldcfg.load(os.path.join(os.path.dirname(os.path.realpath(__file__)),"qcore.cfg"))
-# TODO: this is hardcoded
-gen_model_cords_bin = os.path.join(qcore_cfg['tools_dir'],'gen_model_cords_bin')  #gen_model_cords_bin = '/nesi/projects/nesi00213/tools/gen_model_cords'
-
-try:
-    from params import *
-except ImportError:
-    print "params.py is not available"
-    print "Try params_base.py"
-    try:
-        from params_base import *
-
-    except ImportError:
-        print "Try params_vel.py"
-        try:
-            from params_vel import *
-        except ImportError:
-            print "Error: params_vel.py is missing"
-            sys.exit()
-
-#variables that cannot be found in params_base.py
+GEN_CORD_BIN = "/nesi/projects/nesi00213/tools/gen_model_cords"
+# hard coded defaults
 CENTER_ORIGIN = '1'
 GEOPROJ = '1'
 DOCOORDS = '1'
-BINDIR = gen_model_cords_bin #Although BINDIR was Null in original cshell, reassigned just in case BINDIR is used in some other scripts.
 
-def main(outdir=None, debug=False):
+# leave main even though it makes no sense unless you change all code calling this
+def main(outdir = '.', debug = False):
 
-    #assign variables according to cord_root    
-    XLEN = float(nx)*float(hh)
-    YLEN = float(ny)*float(hh)
-    ZLEN = float(nz)*float(hh)
-
-    if outdir is not None: #if this program is executed from genDomain, outdir is where all other vel model files will be placed.
-        GRIDFILE = os.path.join(outdir, 'gridfile%s'%sufx)
-        GRIDOUT = os.path.join(outdir, 'gridout%s'%sufx)
-        MODEL_COORDS = os.path.join(outdir, 'model_coords%s'%sufx)
-        MODELPARAMS = os.path.join(outdir, 'model_params%s'%sufx)
-        MODEL_BOUNDS = os.path.join(outdir, 'model_bounds%s'%sufx)
-
-
-    #generate Gridfile
+    # load params for velocity model
+    sys.path.insert(0, outdir)
+    sys.path.append('.')
     try:
-        file_gridfile = open(GRIDFILE, "w")
+        import params_vel as vm
+    except ImportError:
+        try:
+            import params_base as vm
+        except ImportError:
+            import params as vm
+
+    # these are saved as strings, should be floats because that's what they are
+    # YLEN == vm.extent_y etc. in params_vel but not elsewhere
+    XLEN = float(vm.nx) * float(vm.hh)
+    YLEN = float(vm.ny) * float(vm.hh)
+    ZLEN = float(vm.nz) * float(vm.hh)
+
+    GRIDFILE = os.path.join(outdir, 'gridfile%s' % (vm.sufx))
+    GRIDOUT = os.path.join(outdir, 'gridout%s' % (vm.sufx))
+    MODEL_COORDS = os.path.join(outdir, 'model_coords%s' % (vm.sufx))
+    MODELPARAMS = os.path.join(outdir, 'model_params%s' % (vm.sufx))
+    MODEL_BOUNDS = os.path.join(outdir, 'model_bounds%s' % (vm.sufx))
+
+    # generate gridfile
+    try:
+        with open(GRIDFILE, 'w') as gridf:
+            gridf.write("xlen=%f\n" % (XLEN))
+            gridf.write("%10.4f %10.4f %13.6e\n" % (0.0, XLEN, float(vm.hh)))
+            gridf.write("ylen=%f\n" % (YLEN))
+            gridf.write("%10.4f %10.4f %13.6e\n" % (0.0, YLEN, float(vm.hh)))
+            gridf.write("zlen=%f\n" % (ZLEN))
+            gridf.write("%10.4f %10.4f %13.6e\n" % (0.0, ZLEN, float(vm.hh)))
     except IOError:
-        print "cannot open file %s, please check file name or directory"%GRIDFILE
-    else:
-        file_gridfile.write("xlen=%f\n"%XLEN)
-        file_gridfile.write("%10.4f %10.4f %13.6e\n"%(0.0,XLEN,float(hh)))
-        file_gridfile.write("ylen=%f\n"%YLEN)
-        file_gridfile.write("%10.4f %10.4f %13.6e\n"%(0.0,YLEN,float(hh)))
-        file_gridfile.write("zlen=%f\n"%ZLEN)
-        file_gridfile.write("%10.4f %10.4f %13.6e\n"%(0.0,ZLEN,float(hh)))
-        file_gridfile.close()
+        print('Cannot write to location: %s' % (GRIDFILE))
+        sys.exit(1)
 
-        #run gen_model_cords by calling modul subprocess.call()
+    # generate model_params
+    cmd = ("{GEN_CORD_BIN} "
+            "geoproj={GEOPROJ} gridfile='{GRIDFILE}' gridout='{GRIDOUT}' "
+            "center_origin={CENTER_ORIGIN} do_coords={DOCOORDS} "
+            "nzout=1 name='{MODEL_COORDS}' gzip=0 latfirst=0 "
+            "modellon={vm.MODEL_LON} modellat={vm.MODEL_LAT} "
+            "modelrot={vm.MODEL_ROT} 1> '{MODELPARAMS}'") \
+            .format(**dict(locals(), **globals()))
+    if not debug:
+        cmd += ' 2>/dev/null'
+    print(cmd)
+    check_call(cmd, shell = True)
 
-        if debug:
-            errRedirect = ''
-        else:
-            errRedirect = '2>/dev/null'
-        #calling executable "gen_model_cords"
-        #using dictionary for better modification
-        '''
-        cmd = [gen_model_cords_bin, \
-                'geoproj=' + GEOPROJ, 'gridfile=' + GRIDFILE, 'gridout=' + GRIDOUT, \
-                'center_origin=' + CENTER_ORIGIN, 'do_coords=' + DOCOORDS, \
-                'nzout=1', 'name=' + MODEL_COORDS, 'gzip=0', 'latfirst=0', \
-                'modellon=' + MODEL_LON, 'modellat=' + MODEL_LAT, \
-                'modelrot=' + MODEL_ROT, '1>'+ MODELPARAMS, errRedirect
-                ]
-        #exe(cmd,debug)
-        print cmd
-        check_call(cmd, shell=True)
-        '''
-        var_dic = {'BINDIR':BINDIR, 'GEOPROJ':GEOPROJ, 'GRIDFILE':GRIDFILE,'GRIDOUT':GRIDOUT,
-                    'CENTER_ORIGIN':CENTER_ORIGIN,'DOCOORDS':DOCOORDS,
-                    'MODEL_COORDS':MODEL_COORDS,
-                    'MODEL_LON':MODEL_LON,'MODEL_LAT':MODEL_LAT,'MODEL_ROT':MODEL_ROT,'MODELPARAMS':MODELPARAMS,
-                    'errRedirect': errRedirect
-                   } 
-        arg_string = ("{BINDIR} "
-                   "geoproj={GEOPROJ} gridfile={GRIDFILE} gridout={GRIDOUT} "
-                   "center_origin={CENTER_ORIGIN} do_coords={DOCOORDS} "
-                   "nzout=1 name={MODEL_COORDS} gzip=0 latfirst=0 "
-                   "modellon={MODEL_LON} modellat={MODEL_LAT} "
-                   "modelrot={MODEL_ROT} 1> {MODELPARAMS} {errRedirect}").format(**var_dic)
+    # also generate coordinate related outputs
+    if DOCOORDS != '1':
+        return
 
-        print arg_string
-        check_call(arg_string,shell=True)
-        
-        """
-        #legacy code for using call(). whole section has been remade into above
-        #ver 1, long as code, impossible to modify
-        #call("./gen_model_cords geoproj=%s gridfile=%s gridout=%s center_origin=%s do_coords=%s nzout=1 name=%s gzip=0 latfirst=0 modellon=%s modellat=%s modelrot=%s > %s"%(GEOPROJ,GRIDFILE,GRIDOUT,CENTER_ORIGIN,DOCOORDS,COORDFILE,MODEL_LON,MODEL_LAT,MODEL_ROT,PARAMFILE), shell = True)
+    # retrieve MODEL_BOUNDS
+    x_bounds = [0, float(vm.nx) - 1]
+    y_bounds = [0, float(vm.ny) - 1]
+    try:
+        with open(MODEL_COORDS, 'r') as coordf:
+            with open(MODEL_BOUNDS, 'w') as boundf:
+                for line in coordf:
+                    x, y = map(float, line.split()[2:4])
+                    if x in x_bounds or y in y_bounds:
+                        boundf.write(line)
+    except IOError:
+        print('Could not write bounds file.')
+        sys.exit(1)
 
-        #ver2, call can not take multiple arguments "with multiple variables", hence these lines do not work
-        check_call( ["./gen_model_cords gridfile=%s"%GRIDFILE, "geoproj=%s"%GEOPROJ ,"gridout=%s"%GRIDOUT,
-               "center_origin=%s"%CENTER_ORIGIN, "do_coords=%s"%DOCOORDS, 
-               "nzout=1", "name=%s"%COORDFILE, "gzip=0", "latfirst=0", 
-               "modellon=%s"%MODEL_LON, "modellat=%s"%MODEL_LAT, 
-               "modelrot=%s"%MODEL_ROT, ">", "%s"%PARAMFILE
-              ], shell = True )
-        #see codes above this section for modified version
-        """
-        #check
-        if DOCOORDS =='1':
-            #read MODEL_COORDS for read, and MODEL_BOUNDS for write
-            try:
-                file_coordfile = open(MODEL_COORDS, "r")
-            except IOError:
-                print "IO Eorror, cannot open files %s" % MODEL_COORDS
-                sys.exit()
-            try:
-                file_boundfile = open(MODEL_BOUNDS, "w")
-            except IOError:
-                print "IO Eorror, cannot open files %s" % MODEL_BOUNDS
-                sys.exit()
-
-            #read lines from coordfiles
-            while 1:
-                line = file_coordfile.readline()
-                if not line:
-                    break
-                if line[0] == '#':
-                    continue
-                if (float(line.split()[2]) == 0) or (float(line.split()[2]) == float(nx)-1) or (float(line.split()[3]) == 0) or (float(line.split()[3]) == float(ny)-1):
-                    file_boundfile.write(line)
-            file_coordfile.close()
-            file_boundfile.close()
-        #file_base.close()
-        
 if __name__ == "__main__":
-    if len(sys.argv)>1:
-        outdir=sys.argv[1]
+    if len(sys.argv) > 1:
+        main(outdir = sys.argv[1])
     else:
-        outdir=None
-    main(outdir)
+        main()
+
