0c65babba67031e4f988ad3735803d28f35bd3f4
jasonmotha <25143301+jasonmotha@users.noreply.github.com>
2017-11-06 15:50:15 +1300

    Added setup tools and changed structure accordingly

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+pkgs = find_packages()
+print pkgs
+setup(
+    name="qcore",
+    version="1.0",
+    packages=pkgs,
+    url="https://github.com/ucgmsim/qcore",
+    description="QuakeCoRE Library",
+    package_data={'': ['*.json', '*.sh']},
+)
