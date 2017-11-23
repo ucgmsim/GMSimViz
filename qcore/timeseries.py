f94fa0ac87094855c5de3b3575cc17bde1b806e9
jasonmotha <25143301+jasonmotha@users.noreply.github.com>
2017-11-24 11:21:58 +1300

    renamed function and added comment

--- qcore/timeseries.py
+++ qcore/timeseries.py
@@ -153,7 +153,10 @@ def acc2vel(timeseries, dt):
     """
     return np.cumsum(timeseries) * dt
 
-def calcMMI(pgv):
+def pgv2MMI(pgv):
+    """
+    Calculates MMI from pgv based  on Worden et al (2012)
+    """
     return np.where(np.log10(pgv) < 0.53,
                     3.78 + 1.47 * np.log10(pgv),
                     2.89 + 3.16 * np.log10(pgv))
