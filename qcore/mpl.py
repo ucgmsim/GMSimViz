ede4428118cb9aca6276256917032ee84fec17f7
Viktor Polak <viktor.polak@canterbury.ac.nz>
2018-03-26 16:49:39 +1300

    tried to fix the logic

--- qcore/mpl.py
+++ qcore/mpl.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-A collection of common functions used in the plotting scripts.
-
+Functions used in the Matplotlib plotting scripts.
 """
 
 from collections import OrderedDict
@@ -9,57 +8,90 @@ import os
 
 import matplotlib.pyplot as plt
 
-def save_figure(fig, filename, directory = None, png = True, eps = False):
-    """Function to save .png and .eps and print info to stdout.       """
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    path = os.path.abspath(os.path.join(directory, filename))
-    if png:
-        png_filename = path + '.png'
-        fig.savefig(png_filename)
-        print png_filename + ' saved to disk'
-    if eps:
-        eps_filename = path + '.eps'
-        fig.savefig(eps_filename)
-        print eps_filename + ' saved to disk'
+def save_figure(fig, out_dir, basename, png = True, eps = False, \
+        close = False, return_fig = False):
+    """
+    "Shortcut" to run fig.savefig, optionally returns fig.gca().
+    fig: matplotlib figure object
+    out_dir: directory for image output
+    basename: basename for images
+    png: save a png
+    eps: save an eps
+    close: close fig when done
+    """
+    # path is given in components
+    path = os.path.join(out_dir, basename)
 
-def save_and_end_figure(fig, directory, filename, return_figure_handle=False):
-    save_figure(fig, directory, filename)
-    if return_figure_handle:
-        ax = fig.gca()
-        return fig, ax
-    else:
+    # make sure destination dir exists
+    if not os.path.exists(out_dir):
+        try:
+            os.makedirs(out_dir)
+        except OSError:
+            if not os.path.exists(out_dir):
+                raise
+
+    if png:
+        fig.savefig('%s.png' % (path))
+    if eps:
+        fig.savefig('%s.eps' % (path))
+
+    if close:
         plt.close(fig)
-        return None
+
+    # XXX: VERY VERY BAD, LEGACY, DEPRECATED.
+    if return_fig:
+        return fig, fig.gca()
 
 def show_legend(centre=False, extra_labels={}):
+    """
+    TODO: description
+    """
     handles, labels = plt.gca().get_legend_handles_labels()
     by_label = OrderedDict(zip(labels, handles))
     by_label.update(extra_labels)
+
     if centre:
-        plt.legend(by_label.values(), by_label.keys(), loc='upper center', bbox_to_anchor=(0.5, 1.05), mode='expand',
-                    ncol=3, fancybox=True, shadow=True)
+        plt.legend(by_label.values(), by_label.keys(), loc='upper center', \
+                bbox_to_anchor=(0.5, 1.05), mode='expand', \
+                ncol=3, fancybox=True, shadow=True)
     else:
         plt.legend(by_label.values(), by_label.keys(), loc='best', fontsize=9)
 
-def extract_number(input):
-    result = input.split(',')
-    if len(result) <= 1:
-        result = float(result[0])
-    else:
-        result = map(float, result)
-    return result
+def convert_strings_to_floats(string_list):
+    """
+    Questionable logic to map the float function onto a list of strings.
+    List of comma-separated floats for each string are returned
+    BUT if there was one value then the type returned is the single float.
+    """
 
-def convert_string_to_floats(in_text):
-    return [CommonPlot.extract_number(ratio) for ratio in in_text]
+    def extract_numbers(number_string):
+        """
+        Returns all floats in comma seperated number string.
+        """
+        floats = map(float, number_string.split(','))
+        if len(floats) == 1:
+            return floats[0]
+        return floats
 
-def is_hex(s):
+    return map(extract_number, string_list)
+
+def is_virtual_station(station_name):
+    """
+    Checks if all restraints on virtual station names are met.
+    """
+    # 7 characters long
+    if len(station_name) != 7:
+        return False
+
+    # no capitals
+    if sum(map(str.isupper, tuple(station_name))):
+        return False
+
+    # valid hex string
     try:
-        int(s, 16)
-        return True
+        int(station_name, 16)
     except ValueError:
         return False
 
-def is_non_uniform(name):
-    n_caps = sum(1 for c in name if c.isupper())
-    return len(name) == 7 and n_caps == 0 and CommonPlot.is_hex(name)
+    # all tests passed
+    return True
