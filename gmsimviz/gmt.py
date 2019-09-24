f739c0b52fc7d5043f5c5cd132f7efb04d681ed0
Viktor Polak <viktor.polak@canterbury.ac.nz>
2019-09-24 16:38:17 +1200

    sync qcore

--- gmsimviz/gmt.py
+++ gmsimviz/gmt.py
@@ -55,7 +55,7 @@ if not os.path.isdir(GMT_DATA):
     try:
         GMT_DATA = os.environ["GMT_DATA"]
     except KeyError:
-        # GMT_DATA files unavailable
+        # GMT_DATA files unavailable, only needed if using GMT_DATA
         pass
 # LINZ DATA
 LINZ_COAST = {
